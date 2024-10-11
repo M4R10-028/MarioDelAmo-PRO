e9b07e32bdc4d55e8b2e1c21054c789aff5a2317
MarioDelAmo <mario.delamo@cesjuanpablosegundo.es>
2024-10-11 14:31:27 +0200

    update

--- T3/EstructuraFor/src/Ejercicios.java
+++ T3/EstructuraFor/src/Ejercicios.java
@@ -2,9 +2,16 @@ import javax.crypto.spec.PSource;
 import java.util.Scanner;
 
 public class Ejercicios {
-    static Scanner scanner = new Scanner(System.in);
+    public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        //ejercicio1();
+        //ejercicio2();
+        //ejercicio3();
+        ejercicio4();
+    }
+
+    public static void ejercicio2() {
         System.out.println("De que número quieres ver la tabla de multiplicar");
         int n = scanner.nextInt();
 
@@ -13,4 +20,59 @@ public class Ejercicios {
             System.out.println(n + " * " + i + " = " + r);
         }
     }
+
+    public static void ejercicio1() {
+        System.out.println("Cuantas temperaturas quieres introducir");
+        float n = scanner.nextInt();
+
+        float temperatura=0;
+        float sumatorio=0;
+        if (n <= 0){
+            n=10;
+        }
+        for (int i=0; i<n; i++){
+            System.out.printf("Dime la %d temperatura", i+1);
+            temperatura = scanner.nextFloat();
+            sumatorio = sumatorio + temperatura;
+        }
+        float media= sumatorio / n;
+        System.out.printf("La suma de todas las temperaturas es %.2f\nLa media de las temperaturas es de %.2f",sumatorio, media);
+    }
+
+    public static void ejercicio3() {
+        System.out.println("Hasta que numero quieres saber la tabla de multiplicar");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Tabla de multiplicar del " + i + ":");
+            for (int x = 0; x < 11; x++) {
+                int r = i * x;
+                System.out.println("\t" + i + " * " + x + " = " + r);
+            }
+        }
+    }
+
+    public static void ejercicio4() {
+        System.out.println("Desde que numero quieres saber las tablas de multiplicar");
+        int ni = scanner.nextInt();
+
+        System.out.println("Hasta que numero quieres saber la tabla de multiplicar");
+        int n = scanner.nextInt();
+
+        if (ni>=n){
+            System.out.println("No se puede ejecutar");
+        }else{
+
+        for (int i = ni; i < n+1; i++) {
+            System.out.println("Tabla de multiplicar del " + i + ":");
+            for (int x = 0; x < 11; x++) {
+                int r = i * x;
+                System.out.println("\t" + i + " * " + x + " = " + r);
+            }
+        }
+        }
+    }
 }
+
+
+
